045e878e3b195650f8ca20797ca1d2f3e4302b6c
Matt Kuebrich <1854586+MattKuebrich@users.noreply.github.com>
2018-07-09 19:21:10 -0500

    test

--- src/AmateurResearch.cpp
+++ src/AmateurResearch.cpp
@@ -14,9 +14,6 @@ void init(Plugin *p) {
 	p->addModel(modelRectify);
 	p->addModel(modelHzHz);
 	p->addModel(modelEvilVCA);
-	//p->addModel(modelGraffiti);
-	//p->addModel(modelSign);
-	
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+	p->addModel(modelGraffiti);
+	p->addModel(modelSign);
 }
